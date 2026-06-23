1592317ed07f2033b4314f268ce2ed38871f5f1a
Gary Figuerola <garyjfiguerola@yape.com.pe>
2026-06-22 22:25:17 -0500

    [TR-003] | Fix mock port to 3000 | [@garyfimo]
    - Changed the default mock server port from 8080 to 3000 across all environments (mockoon configuration, Koin client config, and Android/iOS platform configuration) to match the running Mockoon instance.

--- shared/src/androidMain/kotlin/com/garyfimo/gogym/Platform.android.kt
+++ shared/src/androidMain/kotlin/com/garyfimo/gogym/Platform.android.kt
@@ -4,7 +4,7 @@ import android.os.Build
 
 class AndroidPlatform : Platform {
     override val name: String = "Android ${Build.VERSION.SDK_INT}"
-    override val apiBaseUrl: String = "http://10.0.2.2:8080"
+    override val apiBaseUrl: String = "http://10.0.2.2:3000"
 }
 
 actual fun getPlatform(): Platform = AndroidPlatform()
